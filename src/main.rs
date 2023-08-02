79ef17a7e773b19baa200b5c0e920669427ecaee
matkrin <matkrin@protonmail.com>
2023-08-02 08:45:53 +0200

    Notifications table

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
 use anyhow::Result;
-use chrono::{DateTime, Local};
+use chrono::Local;
 use clap::{Parser, Subcommand};
-use tabled::{Table, Tabled};
-use mvg_api::{get_routes, get_station, get_departures};
+use mvg_api::{get_departures, get_notifications, get_routes, get_station};
+use tabled::{
+    settings::{object::Columns, Modify, Width},
+    Table, Tabled,
+};
+use terminal_size::{terminal_size, Width as TerminalWidth};
 
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
@@ -46,7 +50,7 @@ enum Commands {
         #[arg(short, long, num_args = 1..)]
         filter: Option<Vec<String>>,
     },
-    
+
     /// Show map in browser
     #[clap(visible_alias = "m")]
     Map {
@@ -59,7 +63,7 @@ enum Commands {
         /// Show the map for night lines
         #[arg(short, long)]
         night: bool,
-    }
+    },
 }
 
 #[tokio::main]
@@ -68,10 +72,27 @@ async fn main() -> Result<()> {
     println!("{:?}", args);
 
     match args.command {
-        Commands::Routes { from, to, time, arrival, } => { handle_routes(from, to, time, arrival).await?; },
-        Commands::Notifications { filter } => { handle_notifications(filter)?; },
-        Commands::Departures { station, offset } => { handle_departures(station, offset).await?; }, 
-        Commands::Map { region, tram, night } => { handle_map(region, tram, night)?; },
+        Commands::Routes {
+            from,
+            to,
+            time,
+            arrival,
+        } => {
+            handle_routes(from, to, time, arrival).await?;
+        }
+        Commands::Notifications { filter } => {
+            handle_notifications(filter).await?;
+        }
+        Commands::Departures { station, offset } => {
+            handle_departures(station, offset).await?;
+        }
+        Commands::Map {
+            region,
+            tram,
+            night,
+        } => {
+            handle_map(region, tram, night)?;
+        }
     };
 
     Ok(())
@@ -93,8 +114,16 @@ struct RouteTableEntry {
     info: String,
 }
 
-async fn handle_routes(from: String, to: String, time: Option<String>, arrival: bool) -> Result<()> {
-    println!("routes with {:?}, {:?}, {:?}, {:?}", from, to, time, arrival);
+async fn handle_routes(
+    from: String,
+    to: String,
+    time: Option<String>,
+    arrival: bool,
+) -> Result<()> {
+    println!(
+        "routes with {:?}, {:?}, {:?}, {:?}",
+        from, to, time, arrival
+    );
     let from = &get_station(&from).await?[0];
     let from_id = match from {
         mvg_api::Location::Station(s) => &s.global_id,
@@ -103,41 +132,59 @@ async fn handle_routes(from: String, to: String, time: Option<String>, arrival: 
     let to = &get_station(&to).await?[0];
     let to_id = match to {
         mvg_api::Location::Station(s) => &s.global_id,
-        _ => todo!()
+        _ => todo!(),
     };
-    let routes = get_routes(from_id, to_id, None , None, None, None, None, None, None).await?;
-    let table_entries = routes.iter().map(|connection| {
-        let origin = &connection.parts[0].from;
-        let destination = &connection.parts[connection.parts.len()-1].to;
-        let time = format!(
-            "{} - {}",
-            origin.planned_departure.format("%H:%M"),
-            destination.planned_departure.format("%H:%M")
-        );
-        let in_minutes = (origin.planned_departure.time() - Local::now().time())
-            .num_minutes()
-            .to_string();
-        let duration = (destination.planned_departure.time() - origin.planned_departure.time())
-            .num_minutes()
-            .to_string();
-        let lines = connection.parts.iter().fold(Vec::new(), |mut acc, x| {
-            acc.push(x.line.label.clone());
-            acc
-        });
-        let lines = lines.iter().map(|x| colorize_line(x)).collect::<Vec<_>>().join(", ");
-        let delay = match origin.departure_delay_in_minutes {
-            Some(delay) if delay != 0 => delay.to_string(),
-            _ => "-".to_string(),
-        };
-        let info = connection.parts.iter().fold(Vec::new(), |mut acc, x| {
-            for message in &x.messages {
-                acc.push(message.clone());
-            }
-            acc
-        }).join("\n");
+    let routes = get_routes(from_id, to_id, None, None, None, None, None, None, None).await?;
+    let table_entries = routes
+        .iter()
+        .map(|connection| {
+            let origin = &connection.parts[0].from;
+            let destination = &connection.parts[connection.parts.len() - 1].to;
+            let time = format!(
+                "{} - {}",
+                origin.planned_departure.format("%H:%M"),
+                destination.planned_departure.format("%H:%M")
+            );
+            let in_minutes = (origin.planned_departure.time() - Local::now().time())
+                .num_minutes()
+                .to_string();
+            let duration = (destination.planned_departure.time() - origin.planned_departure.time())
+                .num_minutes()
+                .to_string();
+            let lines = connection.parts.iter().fold(Vec::new(), |mut acc, x| {
+                acc.push(x.line.label.clone());
+                acc
+            });
+            let lines = lines
+                .iter()
+                .map(|x| colorize_line(x))
+                .collect::<Vec<_>>()
+                .join(", ");
+            let delay = match origin.departure_delay_in_minutes {
+                Some(delay) if delay != 0 => delay.to_string(),
+                _ => "-".to_string(),
+            };
+            let info = connection
+                .parts
+                .iter()
+                .fold(Vec::new(), |mut acc, x| {
+                    for message in &x.messages {
+                        acc.push(message.clone());
+                    }
+                    acc
+                })
+                .join("\n");
 
-        RouteTableEntry { time, in_minutes, duration, lines, delay, info }
-    }).collect::<Vec<_>>();
+            RouteTableEntry {
+                time,
+                in_minutes,
+                duration,
+                lines,
+                delay,
+                info,
+            }
+        })
+        .collect::<Vec<_>>();
 
     let mut table = Table::new(table_entries);
     table.with(tabled::settings::Style::rounded());
@@ -181,11 +228,22 @@ async fn handle_departures(station: String, offset: Option<usize>) -> Result<()>
             Some(min) if min != 0 => min.to_string(),
             _ => "-".to_string(),
         };
-        let info = departure.messages.iter().fold(Vec::new(), |mut acc, x| {
-            acc.push(x.clone());
-            acc
-        }).join("\n");
-        DeparturesTableEntry { time , in_minutes, line, destination, delay, info }
+        let info = departure
+            .messages
+            .iter()
+            .fold(Vec::new(), |mut acc, x| {
+                acc.push(x.clone());
+                acc
+            })
+            .join("\n");
+        DeparturesTableEntry {
+            time,
+            in_minutes,
+            line,
+            destination,
+            delay,
+            info,
+        }
     });
 
     let mut table = Table::new(departures_table_entries);
@@ -205,24 +263,68 @@ struct NotificationsTableEntry {
     details: String,
 }
 
-fn handle_notifications(filter: Option<Vec<String>>) -> Result<()> {
+async fn handle_notifications(filter: Option<Vec<String>>) -> Result<()> {
+    use nu_ansi_term::Style;
     println!("notifications with {:?}", filter);
+    let notifications = get_notifications().await?;
+    // dbg!(&notifications[0]);
+    let notifications_table_entries = notifications.iter().map(|notification| {
+        let lines = notification
+            .lines
+            .iter()
+            .map(|line| line.name.clone())
+            .collect::<Vec<_>>()
+            .join(", ");
+        let duration_from = notification
+            .active_duration
+            .from_date
+            .format("%d.%m.%Y");
+        let duration_to = notification
+            .active_duration
+            .to_date
+            .map(|x| x.format("%d.%m.%Y").to_string())
+            .unwrap_or("".to_string());
+        let duration = format!("{} - {}", duration_from, duration_to);
+        let title = html2text::from_read(notification.title.as_bytes(), 99999);
+        let text = html2text::from_read(notification.text.as_bytes(), 99999);
+        let details = format!("{}\n{}", Style::new().bold().paint(title), text);
+        NotificationsTableEntry {
+            lines,
+            duration,
+            details,
+        }
+    });
+
+    let (TerminalWidth(terminal_width), _) = terminal_size().expect("Not in a terminal");
+    let mut table = Table::new(notifications_table_entries);
+    table
+        .with(tabled::settings::Style::rounded())
+        .with(Modify::new(Columns::first()).with(Width::wrap(10).keep_words()))
+        .with(Modify::new(Columns::last()).with(Width::wrap(terminal_width as usize - 50)));
+    println!("{}", table);
+
     Ok(())
 }
 
 fn handle_map(region: bool, tram: bool, night: bool) -> Result<()> {
     if let (false, false, false) = (region, tram, night) {
-        open::that("https://www.mvg.de/dam/jcr:88249232-e41c-417b-b976-1945c5ade867/netz-tarifplan.pdf")?
+        open::that(
+            "https://www.mvg.de/dam/jcr:88249232-e41c-417b-b976-1945c5ade867/netz-tarifplan.pdf",
+        )?
     };
 
     if region {
-        open::that("https://www.mvg.de/dam/jcr:88249232-e41c-417b-b976-1945c5ade867/netz-tarifplan.pdf")?;
+        open::that(
+            "https://www.mvg.de/dam/jcr:88249232-e41c-417b-b976-1945c5ade867/netz-tarifplan.pdf",
+        )?;
     }
     if tram {
         open::that("https://www.mvg.de/dam/jcr:1164570c-cc5f-4b6d-a007-e99c32b00905/tramnetz.pdf")?;
     }
     if night {
-        open::that("https://www.mvg.de/dam/jcr:fe99cd93-ef1c-483c-a715-f421da96382b/nachtliniennetz.pdf")?;
+        open::that(
+            "https://www.mvg.de/dam/jcr:fe99cd93-ef1c-483c-a715-f421da96382b/nachtliniennetz.pdf",
+        )?;
     }
 
     Ok(())
@@ -256,7 +358,7 @@ fn colorized_ubahn(line: &str) -> String {
             let rhs = Fixed(255).on(Fixed(124)).paint(format!("{} ", rhs));
             let total = [lhs.to_string(), rhs.to_string()].join("");
             Style::new().paint(total)
-        },
+        }
         "U8" => {
             let mut i = line.chars();
             let lhs = i.next().unwrap();
@@ -265,10 +367,9 @@ fn colorized_ubahn(line: &str) -> String {
             let rhs = Fixed(255).on(Fixed(166)).paint(format!("{} ", rhs));
             let total = [lhs.to_string(), rhs.to_string()].join("");
             Style::new().paint(total)
-
-        },
+        }
         _ => Style::default().paint(line),
-    }; 
+    };
     colored.to_string()
 }
 
@@ -290,9 +391,9 @@ fn colorize_sbahn(line: &str) -> String {
             let rhs = Fixed(255).on(Fixed(226)).paint(format!("{} ", rhs));
             let total = [lhs.to_string(), rhs.to_string()].join("");
             Style::new().paint(total)
-        },
+        }
         "S20" => Fixed(255).on(Fixed(203)).paint(format!(" {} ", line)),
         _ => Style::default().paint(line),
-    }; 
+    };
     colored.to_string()
 }
